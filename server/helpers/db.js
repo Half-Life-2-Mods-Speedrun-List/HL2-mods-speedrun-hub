18f5f783fd221ee86c597057f3faec459332009d
Anniina <anniina.leikas55@gmail.com>
2025-03-30 20:18:39 +0300

    -Added authentication.js for handling user registration (and login). Updated db.js to include user registration functionality. Modified db.sql to reflect changes for user registration. Updated index.js to integrate authentication routes.

--- server/helpers/db.js
+++ server/helpers/db.js
@@ -1,18 +1,36 @@
 const { Pool } = require("pg")
-
 require("dotenv").config()
 
-const query = (sql, values = []) => {
-    return new Promise(async(resolve, reject) => {
+// global database connection pool with connection details from environment variables
+const pool = new Pool({
+    user: process.env.POSTGRES_USER,     
+    host: process.env.POSTGRES_HOST,     
+    database: process.env.POSTGRES_DB,  
+    password: process.env.POSTGRES_PASSWORD, 
+    port: process.env.POSTGRES_PORT,     
+  });
+  
+  // function to establish database connection
+  async function connectDB() {
+    try {
+        await pool.connect();
+        console.log('Connected to PostgreSQL');
+    }   catch (error) {
+        console.error('Connection error', error); 
+        process.exit(1); // exit the process if the connection fails
+    }
+  }
+
+  /* new Promise is unnecessary since async/await already returns a promise object*/
+const query = async (sql, values = []) => {
         try {
-            const pool = openDb()
-            const result = await pool.query(sql, values)
-            resolve(result)
-        }catch(error) {
-            reject(error.message)
+            return await pool.query(sql, values)
+        } catch(error) {
+            throw new Error(error.message)
         }
-    })
-}
+    }
+
+/* I don't think there's need for creating new connection every time
 
 const openDb = () => {
     const pool = new Pool({
@@ -23,8 +41,8 @@ const openDb = () => {
         port: process.env.POSTGRES_PORT,
     })
     return pool;
-}
+}*/
 
 module.exports = {
-    query
+    query, connectDB,
 }
--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
-require("dotenv").config()
+require('dotenv').config();
 const express = require("express")
 const cors = require("cors")
+const bcrypt = require("bcrypt")
+const { authRouter} = require("./routes/authentication.js")
 const cookieParser = require("cookie-parser")
 const { modRouter } = require("./routes/mod.js")
 const { userRouter } = require("./routes/user.js")
-require('dotenv').config();
+const { connectDB } = require("./helpers/db.js");
 
 const app = express()
 
@@ -17,11 +19,20 @@ app.use(express.urlencoded({extended: false}))
 
 app.use("/mods", modRouter)
 app.use("/user", userRouter)
+app.use("/auth", authRouter)
+
 
 app.get('/', (req, res) => {
     res.send('change the address to find content');
   });
 
+  async function startServer() {
+    await connectDB();
 
-const port = process.env.PORT
-app.listen(port)
+    const port = process.env.PORT || 3001;
+    app.listen(port, function() {
+      console.log(`Server is running on ${port}`);
+    });
+  }
+
+startServer();
