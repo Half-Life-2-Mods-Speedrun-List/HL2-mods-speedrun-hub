64f1c18e05850cb674f6ac929fb4e8addfe94f79
Anniina-55 <anniina.leikas55@gmail.com>
2025-04-14 05:15:47 +0300

    Removed unnecessary origin source from cors configuration

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express()
 
 app.use(cookieParser())
 app.use(cors({
-  origin: ["http://127.0.0.1:5500","http://localhost:5500", "http://127.0.0.1:3000"],
+  origin: ["http://127.0.0.1:5500","http://localhost:5500"],
   credentials: true,
 }))
 app.use(express.json())
