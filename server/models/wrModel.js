81dd98adcb7952adc5220fd9246c0541300647ae
Anniina-55 <anniina.leikas55@gmail.com>
2025-04-15 21:55:42 +0300

    Updated functions to accept and parse correct forms regarding to record time

--- server/models/wrModel.js
+++ server/models/wrModel.js
@@ -4,24 +4,36 @@ const pool = require('../helpers/db.js');
    so the whole time needs to be converted to milliseconds
    to help sorting the times */
 const parseTimeToMilliseconds = (timeString) => {
-        
-    const regex = /(\d+):(\d+)[.:](\d+)/; // Regular expression to match time in the given format, eg."07:23:122" -> 3 groups (min, sec, ms)
+    // Regular expression to match time in the given format, eg."01:07:23.122" -> 4 groups (hour, min, sec, ms)
+    const regex = /^(\d{1,2}:)?(\d{1,2}):(\d{2})[.:](\d{1,3})$/; // accepts either HH:MM:SS.SSS or MM:SS.SSS
     const match = timeString.match(regex); // applaying regex to given parameter to extract time into separate parts
         if (!match) return null; // if input doesn't match
-    // converting to integers. Fully matched string is match[0], eg. "07:23:122"       
-    const minutes = parseInt(match[1], 10); // takes minutes from the time as first group (substring), base is decimal (10)
-    const seconds = parseInt(match[2], 10); // seconds as second group
-    const milliseconds = parseInt(match[3], 10); // milliseconds as third group
+    // converting to integers 
+    const hours = match[1] ? parseInt(match[1].replace(':', ''), 10) : 0;       
+    const minutes = parseInt(match[2], 10); // takes minutes from the time as first group (substring), base is decimal (10)
+    const seconds = parseInt(match[3], 10); // seconds as second group
+    const milliseconds = parseInt(match[4], 10); // milliseconds as third group
+
+    // check of correct formats
+    if (minutes > 59 || seconds > 59 || milliseconds > 999) return null;
+
     // calculating everything into milliseconds
-    return (minutes * 60 * 1000) + (seconds * 1000) + milliseconds;
+    return (hours * 3600000) + (minutes * 60 * 1000) + (seconds * 1000) + milliseconds;
 }
 
 const formatMillisecondsToTime = (milliseconds) => {
-    const minutes = Math.floor(milliseconds / 60000); // calculating full minutes (1 min has 60000 seconds)
+    const hours = Math.floor(milliseconds / 3600000); // calcultaing full hours (1 h has 3 600 000 milliseconds)
+    const minutes = Math.floor((milliseconds % 3600000) / 60000); // calculating full minutes (1 min has 60000 seconds)
     const seconds = Math.floor((milliseconds % 60000) / 1000); // remainder from full minutes is calculated by 1000 since i second has 1000 ms
     const ms = milliseconds % 1000; // leftover milliseconds 
-    // finally padding minutes, seconds and milliseconds with leading zeros        
-    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}.${String(ms).padStart(3, '0')}`;
+
+    const timeStr = [
+        hours > 0 ? String(hours) : null, // add hours only if they are included in the input
+        String(minutes).padStart(2, '0'),
+        String(seconds).padStart(2, '0')
+    ].filter(Boolean).join(':')
+    // finally padding milliseconds with leading zeros if needed       
+    return `${timeStr}.${String(ms).padStart(3, '0')}`
 };
 
 const getWRHistory = async (categoryId) => {
