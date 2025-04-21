879599e562f016ec520c0af1c84f425ef4c38248
Anniina-55 <anniina.leikas55@gmail.com>
2025-04-21 21:55:58 +0300

    Fixed category_id bug

--- server/models/gameCategoryModel.js
+++ server/models/gameCategoryModel.js
@@ -1,47 +1,39 @@
 const pool = require("../helpers/db.js");
 
-const createCategory = async (name, mod_id, user_id) => {
-    console.log("Creating category with name:", name, "and mod_id", mod_id)
+const createCategory = async (category_name, mod_id, user_id) => {
+    console.log("Creating category with name:", category_name, "and mod_id", mod_id)
     
     try {
         console.log("Starting transaction")
         await pool.query("BEGIN");
 
         // check if category already exists
-        const existingCategory = await pool.query('SELECT * FROM categories WHERE category_name=$1', [name]);
+        const existingCategory = await pool.query(
+            `SELECT c.category_id FROM categories c 
+            JOIN mod_category mc ON c.category_id=mc.category_id
+            WHERE c.category_name = $1 AND mc.mod_id=$2`, [category_name, mod_id]);
         console.log("Existing categories:", existingCategory.rows)
-        let categoryId;
 
         if (existingCategory.rows.length > 0) {
-            categoryId = existingCategory.rows[0].category_id;
-            console.log("Category exists, ID:", categoryId);
+            console.log("Category already exists in this mod");
+            await pool.query("ROLLBACK");
+            throw new Error("Category already exists in this mod");
         } 
         // adding category to categories-table 
-        else {
             const categoryResult = await pool.query(
                 "INSERT INTO categories (category_name, user_id) VALUES ($1, $2) RETURNING *",
-                [name, user_id]
-            )
-            categoryId = categoryResult.rows[0].category_id;
+                [category_name, user_id])
+        
+            const categoryId = categoryResult.rows[0].category_id;
             console.log("Inserted new category with ID:", categoryId);
-        }
-        // check if category is already linked to mod
-        console.log("Inserting into mod_category table with modId:", mod_id, "and with categoryId", categoryId)
-        const existingModCategory = await pool.query(
-            "SELECT * FROM mod_category WHERE mod_id = $1 AND category_id = $2",
-            [mod_id, categoryId]
-        );
-
-        if (existingModCategory.rows.length > 0) {
-            throw new Error("Category already linked to this mod");
-        }
+        
         // adding to mod_category table
         await pool.query(
             "INSERT INTO mod_category (mod_id, category_id) VALUES ($1, $2) RETURNING *",
             [mod_id, categoryId]
         )
         await pool.query("COMMIT") // commit saves data to database
-        return {id: categoryId, name};
+        return {id: categoryId, category_name};
     } catch (error) {
     // if there's error with the query, rollback cancels all previous actions
         await pool.query("ROLLBACK")
