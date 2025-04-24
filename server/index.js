a76121408ac57a51a400e9677f4c27ee64df4dd8
emarkula24 <emarkula24@student.oulu.fi>
2025-04-24 19:38:57 +0300

    edited cors permissions

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express()
 
 app.use(cookieParser())
 app.use(cors({
-  origin: ["http://127.0.0.1:5500","http://localhost:5500"],
+  origin: ["http://127.0.0.1:5500","http://localhost:5500", "https://hl2-mods-speedrun-hub.onrender.com"],
   credentials: true,
 }))
 app.use(express.json())
